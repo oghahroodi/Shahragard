03f83fa3b5ba7a736d325c7238c5aaf0cafea15a
Fatima <oghahroodi98@gmail.com>
2018-12-06 16:21:40 +0330

    feat: Add trip model to trip app footer: https://trello.com/c/AqHCvumg

--- shahragard/trip/models.py
+++ shahragard/trip/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+class Trip(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_time = models.CharField(max_length=50, null=False)
+    origin = models.CharField(max_length=50, null=False)
+    destination = models.CharField(max_length=50, null=False)
+    cost = models.CharField(max_length=50, null=False)
+    number_of_passengers = models.IntegerField(null=False)
