2e9ef1882b8107d2ebf29530109fff85854ccd3d
Fatima <oghahroodi98@gmail.com>
2018-12-06 17:45:11 +0330

    feat: Add editprofile api to project footer: https://trello.com/c/bHKRgqU6

--- shahragard/user/urls.py
+++ shahragard/user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenVerifyView
+from . import views
 
 urlpatterns = [
     path('apiv1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('apiv1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('apiv1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('apiv1/edit/', views.Edit.as_view()),
 ]
--- shahragard/user/views.py
+++ shahragard/user/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from rest_framework.views import APIView
+from rest_framework import status
+from .serializers import *
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+from .models import Person
 
-# Create your views here.
+
+class Edit(APIView):
+    def patch(self, request):
+        email = request.data.get("email")
+        name = request.data.get("name")
+        car = request.data.get("car")
+        plaque = request.data.get("plaque")
+        person = Person.objects.get(user__id=request.user.id)
+        person.name = name
+        person.email = email
+        person.car = car
+        person.plaque = plaque
+        person.save()
+        return JsonResponse({"status": "200"})
