91dc17416878690cd0a51f008765cf8191b3fca1
Fatima <sf.mirashrafi@gmail.com>
2018-12-06 22:38:48 +0330

    feat: Add request to trip model

--- shahragard/trip/models.py
+++ shahragard/trip/models.py
@@ -9,3 +9,10 @@ class Trip(models.Model):
     destination = models.CharField(max_length=50, null=False)
     cost = models.CharField(max_length=50, null=False)
     number_of_passengers = models.IntegerField(null=False)
+
+
+class RequestTrip(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+    accept = models.BooleanField(default=False)
+    number_of_passengers = models.IntegerField(null=False)
