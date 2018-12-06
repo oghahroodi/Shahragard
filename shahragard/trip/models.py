36a3a5780a982012cd1de787a4ee715e9e2281be
Fatima <oghahroodi98@gmail.com>
2018-12-06 19:25:53 +0330

    fix: Use pep8 standard

--- shahragard/trip/models.py
+++ shahragard/trip/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from user.models import User
 
+
 class Trip(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     start_time = models.CharField(max_length=50, null=False)
