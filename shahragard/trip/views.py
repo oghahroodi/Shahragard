f3d858abbea3e760bae40286a9f18574f6044278
Fatima <oghahroodi98@gmail.com>
2018-12-06 16:26:00 +0330

    feat: Add make trip api to trip app footer: https://trello.com/c/AqHCvumg

--- shahragard/trip/views.py
+++ shahragard/trip/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status
+from .seializers import *
+from user.models import *
+from user.models import User, Person
+from django.http import JsonResponse
 
-# Create your views here.
+
+class TripHandler(APIView):
+
+    def post(self, request, *args, **kwargs):
+        print(request.data)
+        userid = self.request.user.id
+        request.data['user'] = userid
+        serializer = MakeTripSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse({'status': 'CREATED'}, status=status.HTTP_201_CREATED)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
