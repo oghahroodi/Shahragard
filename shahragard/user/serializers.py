df2894918f7a30f17fe10fb30719929e7f9523d9
Fatima <sf.mirashrafi@gmail.com>
2018-12-08 13:04:36 +0330

    feat: Add RequestTrip Serializer

--- shahragard/user/serializers.py
+++ shahragard/user/serializers.py
@@ -1,7 +1,7 @@
 from .models import *
 from rest_framework import serializers
 from django.contrib.auth.models import User
-
+from trip.models import RequestTrip
 
 class UserSerializer(serializers.ModelSerializer):
 
@@ -22,3 +22,26 @@ class ProfilePageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
         fields = ('user', 'name', 'phone_number', 'email', 'car', 'plaque')
+
+
+class RequestTripSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RequestTrip
+        fields = ('start_time', 'origin', 'destination', 'cost')
+
+        def get_start_time(self, obj):
+            start_time = RequestTrip.objects.get(id=obj.trip.start_time)
+            return start_time
+
+        def get_origin(self, obj):
+            origin = RequestTrip.objects.get(id=obj.trip.origin)
+            return origin
+
+        def get_destination(self, obj):
+            destination = RequestTrip.objects.get(id=obj.trip.destination)
+            return destination
+
+        def get_number_of_passengers(self, obj):
+            number_of_passengers = RequestTrip.objects.get(id=obj.trip.number_of_passengers)
+            return number_of_passengers
+
