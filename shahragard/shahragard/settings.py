6f6b51b31d2a25bae8556a91e87f9f38f5c27814
Omid Ghahroodi <oghahroodi98@gmail.com>
2018-12-20 16:54:44 +0330

    feat: Add logging to login-signup

--- shahragard/shahragard/settings.py
+++ shahragard/shahragard/settings.py
@@ -1,3 +1,5 @@
+from django.utils.log import DEFAULT_LOGGING
+import logging
 """
 Django settings for shahragard project.
 
@@ -166,3 +168,32 @@ ROLLBAR = {
 }
 rollbar.init(**ROLLBAR)
 # rollbar.report_message("Hello world")
+
+LOGGING_CONFIG = None
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'console',
+        },
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': 'logs.log',
+
+        }
+    },
+    'loggers': {
+        '': {
+            'level': 'INFO',
+            'handlers': ['console', 'file'],
+        },
+    },
+})
