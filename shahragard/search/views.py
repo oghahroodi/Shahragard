4b85e0f1d7d72f41303d95fd5d1a3c7e02204666
Omid Ghahroodi <oghahroodi98@gmail.com>
2018-12-20 14:01:57 +0330

    fix: Move search validator to static method and make const file

--- shahragard/search/views.py
+++ shahragard/search/views.py
@@ -1,5 +1,6 @@
 import os
 import json
+from search.const import Const
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework import generics, status
@@ -14,49 +15,20 @@ from json import loads, dumps
 class SearchTrips(APIView):
     def post(self, request):
         userid = self.request.user.id
+
         try:
             request.data['user'] = userid
             start_time = request.data['start_time']
             origin = request.data['origin']
             destination = request.data['destination']
             number_of_passengers = request.data['number_of_passengers']
-        except:
+        except KeyError:
             return JsonResponse({'status': 'give valid data'},
                                 status=status.HTTP_400_BAD_REQUEST)
-        if (len(request.data.get('start_time')) != 6 and
-                len(request.data.get('start_time')) != 0):
-            return JsonResponse(
-                {'status': 'pls enter valid time'},
-                status=status.HTTP_400_BAD_REQUEST)
-        for i in request.data.get('start_time'):
-            if not(i.isdigit()):
-                return JsonResponse(
-                    {'status': 'pls enter valid time'},
-                    status=status.HTTP_400_BAD_REQUEST)
-        f = open(os.path.dirname(__file__) + '/../../json_shahr.json').read()
-        data = json.loads(f)
-        tmp1 = False
-        tmp2 = False
-        if request.data['origin'] == '':
-            tmp1 = True
-        else:
-            for i in data.values():
-                if request.data['origin'] == i:
-                    tmp1 = True
-                    break
-        if request.data['destination'] == '':
-            tmp2 = True
-        else:
-            for i in data.values():
-                if request.data['destination'] == i:
-                    tmp2 = True
-                    break
-        if not(tmp1 and tmp2):
-            return JsonResponse(
-                {'status': 'pls enter valid city'},
-                status=status.HTTP_400_BAD_REQUEST)
+
+        SearchTrips.validator(request)
         query = Q()
-        number_of_passengers = request.data['number_of_passengers']
+
         if start_time != '':
             query = query & Q(start_time=start_time)
         if origin != '':
@@ -65,8 +37,49 @@ class SearchTrips(APIView):
             query = query & Q(destination=destination)
         if number_of_passengers != '':
             query = query & Q(number_of_passengers=number_of_passengers)
+
         q = Trip.objects.filter(query)
         serializer = TripSerializer(q, many=True, context={
             "userid": request.user.id})
         return JsonResponse({'res': loads(dumps(serializer.data))},
                             status=status.HTTP_200_OK)
+
+    @staticmethod
+    def validator(request):
+        if (len(request.data.get('start_time')) != 6 and
+                len(request.data.get('start_time')) != 0):
+            return JsonResponse(
+                {'status': 'pls enter valid time'},
+                status=status.HTTP_400_BAD_REQUEST)
+
+        for i in request.data.get('start_time'):
+            if not(i.isdigit()):
+                return JsonResponse(
+                    {'status': 'pls enter valid time'},
+                    status=status.HTTP_400_BAD_REQUEST)
+
+        f = open(os.path.dirname(__file__) + Const.shahr).read()
+        data = json.loads(f)
+        tmp1 = False
+        tmp2 = False
+
+        if request.data['origin'] == '':
+            tmp1 = True
+        else:
+            for i in data.values():
+                if request.data['origin'] == i:
+                    tmp1 = True
+                    break
+
+        if request.data['destination'] == '':
+            tmp2 = True
+        else:
+            for i in data.values():
+                if request.data['destination'] == i:
+                    tmp2 = True
+                    break
+
+        if not(tmp1 and tmp2):
+            return JsonResponse(
+                {'status': 'pls enter valid city'},
+                status=status.HTTP_400_BAD_REQUEST)
