56595a442e70ba4e6ecf0c9b8bd3c6824b13dcab
Omid Ghahroodi <oghahroodi98@gmail.com>
2019-01-28 06:28:24 +0330

    feat: Add post and put api for commenting for comment and get comment

--- shahragard/trip/admin.py
+++ shahragard/trip/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Trip, RequestTrip
+from .models import Trip, RequestTrip, Comment
 
 admin.site.register(Trip)
 admin.site.register(RequestTrip)
+admin.site.register(Comment)
